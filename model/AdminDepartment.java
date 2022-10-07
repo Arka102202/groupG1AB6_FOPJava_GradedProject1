4cc5fa311091908d6c012e4811005916c4dd3339
Arka102202 <111365238+Arka102202@users.noreply.github.com>
2022-10-07 13:23:50 +0530

    committing java files for model and driver packages

---
+++ model/AdminDepartment.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * @author Arkadyuti Sikdar
+ */
+
+
+public class AdminDepartment extends SuperDepartment {
+
+    public String departmentName(){
+        return "Admin Department";
+    }
+
+    public String getTodaysWork(){
+        return "Complete your documents Submission";
+    }
+
+    public String getWorkDeadline(){
+        return "Complete by EOD";
+    }
+
+}
---
+++ model/TechDepartment.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * @author Supratim Roy
+ */
+
+
+public class TechDepartment extends SuperDepartment{
+
+    public String departmentName() {
+        return "Tech Department";
+    }
+
+    public String getTodaysWork() {
+        return "Complete Coding of Module 1";
+    }
+
+    public String getWorkDeadline() {
+        return "Complete by EOD";
+    }
+
+    public String getTechStackInformation() {
+        return "Core Java";
+    }
+}
